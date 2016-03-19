58500797480859e31df969925b0f7ad9fc28d067
LombaxPieboy <Brandon.Ogon@gmail.com>
2016-03-18 23:44:45 -0300

    A bunch of conversation stuff

---
+++ Assets/Scripts/ConvButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConvButton : MonoBehaviour {
+    
+    Conversation conv;
+
+	// Use this for initialization
+	void Start () {
+        conv = GameObject.Find("Mushman").GetComponent<Conversation>();
+        if (this.name == "ButtonNext") {
+            this.GetComponentInChildren<Text>().text = "Next";
+        } else if (this.name == "ButtonConv1") {
+           this.GetComponentInChildren<Text>().text = conv.getOptions(0);
+        } else if (this.name == "ButtonConv2") {
+           this.GetComponentInChildren<Text>().text = conv.getOptions(1);
+        }
+        this.GetComponent<Button>().onClick.AddListener(delegate {this.clicked(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void clicked() {
+        if (this.name == "ButtonNext") {
+            conv.increment();
+        } else if (this.name == "ButtonConv1") {
+            conv.switchActive(1);
+            Destroy(this);
+        } else if (this.name == "ButtonConv2") {
+            conv.switchActive(2);
+            Destroy(this);
+        }
+    }
+}
